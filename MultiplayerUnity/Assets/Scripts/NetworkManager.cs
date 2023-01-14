488fb4153d155d59e30edcac682638195d7e07cd
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-13 23:23:38 -0500

    Room screens done

--- MultiplayerUnity/Assets/Scripts/NetworkManager.cs
+++ MultiplayerUnity/Assets/Scripts/NetworkManager.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class NetworkManager: MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField roomName;
+    [SerializeField] TMP_Text errorText;
+
     public void Start()
     {
         Debug.Log("Connecting to Master");
@@ -23,8 +27,23 @@ public class NetworkManager: MonoBehaviourPunCallbacks
         Debug.Log("Joined Lobby");
     }
 
-    public void Update()
+    public void CreateRoom()
     {
-        
+        if (string.IsNullOrEmpty(roomName.text))
+            return;
+
+        PhotonNetwork.CreateRoom(roomName.text);
+        ScreenManager.Instance.DisplayScreen("Main");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        ScreenManager.Instance.DisplayScreen("Room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + errorText;
+        ScreenManager.Instance.DisplayScreen("Error");
     }
 }
