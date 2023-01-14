14767f278c869e0d11baf61dfafcf3286213621a
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-13 22:40:52 -0500

    Add basic network joining and start the join UI

---
+++ MultiplayerUnity/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager: MonoBehaviourPunCallbacks
+{
+    public void Start()
+    {
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+    public void Update()
+    {
+        
+    }
+}
