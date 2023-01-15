c512bdf1d1de4156796a6ef97634d123d377e9ff
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-15 14:18:11 -0500

    Game works for one player

--- MultiplayerUnity/Assets/Scripts/PlayerController.cs
+++ MultiplayerUnity/Assets/Scripts/PlayerController.cs
@@ -2,33 +2,142 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 using TMPro;
 
 public class PlayerController : MonoBehaviourPunCallbacks
 {
     PhotonView PV;
 
-    [SerializeField] TMP_Text text;
+    public Button revealBtn;
+    public Button winCard;
+    public Button loseCard;
+
+    List<string> allPhrases = new List<string>();
+
+    //need to sync
+    public int cardNum;
+    public string phrase;
+
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI displayCard;
+    public TextMeshProUGUI displayPhrase;
 
     void Awake()
     {
         PV = GetComponent<PhotonView>();
 
-        transform.parent = GameObject.Find("Canvas").transform;
+        transform.SetParent(GameObject.Find("Canvas").transform);
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        cardNum = 5;
+
+        playerName.text = PhotonNetwork.NickName;
+
+        createList();
+        generatePhrase();
     }
 
-    public void generate()
+    void createList()
     {
-        if (PV.IsMine)
-        {
-            PV.RPC("updateText", RpcTarget.AllBuffered, Random.Range(0, 1000).ToString());
-        }
+        //look
+        allPhrases.Add("Look Up");
+        allPhrases.Add("Look Down");
+        allPhrases.Add("Look Back");
+
+        //do
+        allPhrases.Add("Hesitate");
+        allPhrases.Add("Compare");
+        allPhrases.Add("Ask");
+        allPhrases.Add("Laugh");
+        allPhrases.Add("Stand");
+
+        allPhrases.Add("Help");
+        allPhrases.Add("Point using finger");
+        allPhrases.Add("Recall");
+
+        //say
+        allPhrases.Add("Agree");
+        allPhrases.Add("Disagree");
+
+        allPhrases.Add("Reject to answer");
+
+        allPhrases.Add("Repeat");
+
+        allPhrases.Add("Guess the phrase");
+        allPhrases.Add("Praise");
+        allPhrases.Add("Touch head");
+
+        //speak
+        allPhrases.Add("Talk about time");
+
+        allPhrases.Add("Say any name");
+        allPhrases.Add("Say any food");
+        allPhrases.Add("Say any subject");
+        allPhrases.Add("Say any animal");
+
+        allPhrases.Add("Say 'If'");
+        allPhrases.Add("Say 'No'");
+        allPhrases.Add("Say 'Unbelievable'");
+    }
+
+    public void reveal()
+    {
+        if (!PV.IsMine) return;
+
+        winCard.gameObject.SetActive(true);
+        loseCard.gameObject.SetActive(true);
+        displayPhrase.gameObject.SetActive(true);
+        revealBtn.gameObject.SetActive(false);
+    }
+
+    public void win()
+    {
+        if (!PV.IsMine) return;
+
+        cardNum++;
+
+        generatePhrase();
+    }
+
+    public void lose()
+    {
+        if (!PV.IsMine) return;
+
+        cardNum--;
+
+        generatePhrase();
+    }
+
+    public void generatePhrase()
+    {
+        if (!PV.IsMine) return;
+
+        phrase = allPhrases[Random.Range(0, allPhrases.Count)];
+
+        PV.RPC("updatePhrase", RpcTarget.AllBuffered, cardNum, phrase);
     }
 
     [PunRPC]
-    void updateText(string number)
+    void updatePhrase(int number, string phrase)
     {
-        text.text = number;
+        displayCard.text = "Cards Left: " + number;
+        displayPhrase.text = phrase;
+
+        winCard.gameObject.SetActive(false);
+        loseCard.gameObject.SetActive(false);
+
+        if (PV.IsMine)
+        {
+            revealBtn.gameObject.SetActive(true);
+            displayPhrase.gameObject.SetActive(false);
+        }
+        else
+        {
+            revealBtn.gameObject.SetActive(false);
+            displayPhrase.gameObject.SetActive(true);
+        }
     }
 }
 
