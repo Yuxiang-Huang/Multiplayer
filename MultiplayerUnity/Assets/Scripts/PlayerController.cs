216f11e8bed5947318cedde12b1f3170e0c78aec
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-15 13:13:13 -0500

    Able to sync number across server

--- MultiplayerUnity/Assets/Scripts/PlayerController.cs
+++ MultiplayerUnity/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerController : MonoBehaviourPunCallbacks
 {
     PhotonView PV;
+
+    [SerializeField] TMP_Text text;
 
     void Awake()
     {
@@ -13,4 +16,19 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         transform.parent = GameObject.Find("Canvas").transform;
     }
+
+    public void generate()
+    {
+        if (PV.IsMine)
+        {
+            PV.RPC("updateText", RpcTarget.AllBuffered, Random.Range(0, 1000).ToString());
+        }
+    }
+
+    [PunRPC]
+    void updateText(string number)
+    {
+        text.text = number;
+    }
 }
+
